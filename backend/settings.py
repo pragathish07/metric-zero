59d8252997e3ab2e4923bb19a60baae9b824df22
pragathish07 <pragarajesh779jd@gmail.com>
2025-02-21 23:04:56 +0530

    fixed prod issue

--- backend/settings.py
+++ backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,10 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL =  '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'backend' / 'static' 
 ]
+
+STATIC_ROOT = str(BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
